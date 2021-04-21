2b0b489ed61779769f6b83be7bf2846701852eff
KTELLAWI <koutaiba@msn.com>
2021-04-21 22:28:01 +0300

    key

--- key.js
+++ key.js
@@ -1,9 +1,10 @@
 //API KEY  https://developers.google.com/custom-search/v1/using_rest
 //context key- 
 
-export const API_KEY ='AIzaSyCFdUJXp0Cs14sdADlnxmhEamYFG89aLAg'
+export const API_KEY ='AIzaSyDgJ_oIyJzLZ-aPtVd9EpVlXwCAEsXvltE'
 export const CONTEXT_KEY='1fb7a0f318f2917b1'
 
 
 
  //https://www.googleapis.com/customsearch/v1?key=AIzaSyCFdUJXp0Cs14sdADlnxmhEamYFG89aLAg&cx=1fb7a0f318f2917b1&q=ddd
+ AIzaSyDgJ_oIyJzLZ-aPtVd9EpVlXwCAEsXvltE
